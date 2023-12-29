76211e06ef20f5f424d17744355c4e266ff3beb9
Hussein Najah <husseinnajah123@gmail.com>
2023-12-29 23:08:47 +0300

    Add variant="primary" to Navbar button

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -14,6 +14,7 @@ export const Navbar = () => {
         </div>
         <Button
           size="sm"
+          variant="primary"
           className="rounded-sm md:block py-1.5 hidden h-auto px-2"
         >
           Create
