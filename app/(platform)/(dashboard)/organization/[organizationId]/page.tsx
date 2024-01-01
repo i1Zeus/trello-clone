a3d5e0d868b04d2ff0eed1d1acb23e8d34cfb815
Hussein Najah <husseinnajah123@gmail.com>
2024-01-01 23:25:06 +0300

    Refactor organization page component

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,16 +1,9 @@
-import { FormInput } from "@/components/form/form-input";
-import { FormSubmit } from "@/components/form/form-submit";
+import { Info } from "./_components/info";
 
 const OrganizationIdPage = () => {
-  const onSubmit = () => {
-    "use server";
-  };
   return (
-    <div>
-      <form action={onSubmit}>
-        <FormInput id="title" label="Board Title" />
-        <FormSubmit>Save</FormSubmit>
-      </form>
+    <div className="w-full mb-20">
+      <Info />
     </div>
   );
 };
